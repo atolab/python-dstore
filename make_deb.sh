4f033f8935d53a3c7912448267e23033daabd530
gabrik <gabriele.baldoni@gmail.com>
2018-05-16 11:33:48 +0200

    FIX: make_deb.sh added flag for python3 only and shebang

--- make_deb.sh
+++ make_deb.sh
@@ -1,11 +1,11 @@
-
+#!/usr/bin/env bash
 
 $WD=$(PWD)
 
 python3 setup.py sdist
 
 cd dist
-py2dsc dstore-0.1.0.tar.gz
+py2dsc --with-python3 True --with-python2 False dstore-0.1.0.tar.gz
 cd deb_dist/dstore-0.1.0
 dpkg-buildpackage -rfakeroot -uc -us
 
