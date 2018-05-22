aaad76bac56f3af8c14c6cbf4032bcac3aea9944
gabrik <gabriele.baldoni@gmail.com>
2018-05-22 14:41:57 +0200

    updated make_deb.sh

--- make_deb.sh
+++ make_deb.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-$WD=$(PWD)
-
+#$WD=$(PWD)
+rm -r dist
 python3 setup.py sdist
 
 cd dist
-py2dsc --with-python3 True --with-python2 False dstore-0.1.0.tar.gz
-cd deb_dist/dstore-0.1.0
+py2dsc --with-python3 True --with-python2 False dstore-0.1.1.tar.gz
+cd deb_dist/dstore-0.1.1
 dpkg-buildpackage -rfakeroot -uc -us
 
-cp ../python-dstore_0.1.0-1_all.deb $WD
+#cp ../python-dstore_0.1.0-1_all.deb $WD
